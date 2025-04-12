63b9eff5d4a42442068c95f63bc1334b7c189175
Parikshit <Parikshitmali17@gmail.com>
2025-04-12 13:21:54 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,41 @@
 
-import React from "react";
-import './../styles/App.css';
+// import React from "react";
+// import './../styles/App.css';
+
+// const App = () => {
+//   return (
+//     <div>
+//         {/* Do not remove the main div */}
+//     </div>
+//   )
+// }
+
+// export default App
+
+import React, { useState } from "react";
+import "./../styles/App.css";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false); // ✅ Correct syntax
+
+  const handleClick = () => {
+    setIsLogin(true); // ✅ use setState to update state
+  };
+
   return (
     <div>
-        {/* Do not remove the main div */}
-    </div>
-  )
-}
+      <label htmlFor="username">User Id</label>
+      <input id="username" type="text" placeholder="User Name" />
+      <br />
+      <br />
+      <label htmlFor="password">Password</label>
+      <input id="password" type="password" placeholder="Password" />
+      <br />
+      <button onClick={handleClick}>Log In</button>
 
-export default App
+      {isLogin && <p>You are now logged in ✅</p>}
+    </div>
+  );
+};
+
+export default App;
